0f0bdaa131488f9661d41e09c0f4fff68484084f
Devdar-hub <darshanbhatt1799@gmail.com>
2026-09-09 12:33:59 +1000

    added gitignore

---
+++ terraform/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_version = ">= 1.7.0"
+
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 4.0"
+        }
+
+        local = {
+            source  = "hashicorp/local"
+            version = "~> 2.5"
+        }
+
+        null = {
+            source  = "hashicorp/null"
+            version = "~> 3.2"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
